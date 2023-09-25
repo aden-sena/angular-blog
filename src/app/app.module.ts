59e2e3cf4e246682d1b54f4c28e17a1e96ebc667
Aden Sena <adennepomuceno@gmail.com>
2023-09-25 15:37:25 -0300

    Componentes adicionados com estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { BannerTitleComponent } from './components/banner-title/banner-title.component';
+import { BannerSubtitleComponent } from './components/banner-subtitle/banner-subtitle.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { NormalCardComponent } from './components/normal-card/normal-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    BannerTitleComponent,
+    BannerSubtitleComponent,
+    BigCardComponent,
+    NormalCardComponent,
+    SmallCardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/banner-subtitle/banner-subtitle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-banner-subtitle',
+  templateUrl: './banner-subtitle.component.html',
+  styleUrls: ['./banner-subtitle.component.css']
+})
+export class BannerSubtitleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
